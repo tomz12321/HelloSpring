7280cabb8df0073b1292a649bd4f5cfa63f630c8
tomz123 <tomz12321@gmail.com>
2017-10-02 13:48:32 +0800

    Kick start of Spring framework (Guestbook)
    A practice of JSP + Spring

---
+++ target/Guestbook/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : 28/09/2017, 9:01:56 PM
+    Author     : tomz123
+--%>
+
+<%@page contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@page import="guest.*"%>
+
+<jsp:forward page="guest.html" />
+
+<html>
+    <head>
+        <title>JPA Guestbook Web Application Tutorial</title>
+    </head>
+ 
+    <body>
+        Hello World!
+    </body>
+ </html>
---
+++ target/generated-sources/annotations/guest/Guest_.java
@@ -0,0 +1,16 @@
+package guest;
+
+import java.sql.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-02T13:37:25")
+@StaticMetamodel(Guest.class)
+public class Guest_ { 
+
+    public static volatile SingularAttribute<Guest, Date> signingDate;
+    public static volatile SingularAttribute<Guest, String> name;
+    public static volatile SingularAttribute<Guest, Long> id;
+
+}
