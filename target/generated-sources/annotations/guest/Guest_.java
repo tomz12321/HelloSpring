4c45620dd0927424772daabb870539f03c6a6e64
tomz123 <tomz12321@gmail.com>
2018-04-14 22:40:42 +1000

    Submit .gitignore for HelloSpring Project
    Submit .gitignore for HelloSpring Project
    Ignore most of the files in Netbeans IDE

--- target/generated-sources/annotations/guest/Guest_.java
+++ target/generated-sources/annotations/guest/Guest_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-02T13:37:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-25T20:11:03")
 @StaticMetamodel(Guest.class)
 public class Guest_ { 
 
